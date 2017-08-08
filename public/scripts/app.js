443ce7c6bff9a6672f1d21550e9bb6257501fcde
Amy <amy@lighthouselabs.ca>
2017-08-08 21:12:39 +0000

    Added AJAX get/post to fetch tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,50 +1,3 @@
-var data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1502211510181
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "<script>alert('uh oh!');</script>"
-    },
-    "created_at": 1461113796368
-  }
-];
-
 function escape(str) {
   var div = document.createElement('div');
   div.appendChild(document.createTextNode(str));
@@ -81,21 +34,43 @@ const generateHTML = (obj) => {
 //DOM READY
 $(function() {
 
-
-const renderTweet= (data) => {
-  var articles = data.map(generateHTML)
-  var html = articles.join('')
+//RENDER THE CORRECT HTML FOR TWEETS
+const renderTweets = (data) => {
+  var html = data
+            .sort((a,b) => b.created_at - a.created_at)
+            .map(generateHTML)
+            .join('')
   $('#tweets-container').html(html)
  }
 
 
- renderTweet(data)
+//LOAD TWEETS ON PAGE LOAD
+const loadTweets = () => {
+  $.ajax({
+          url:'http://localhost:8080/tweets',
+          method: 'GET'
+         }).then(function (response) {
+            renderTweets(response);
+      });
+  }
+
+//DAT FORM SUBMIT
+$("#tweetform").submit(function(event) {
+  event.preventDefault();
+  var data = $(this).serialize();
+
+   $.ajax({
+        url: 'http://localhost:8080/tweets',
+        method: 'POST',
+        data: data
+      }).then(function() {
+        $('.tweetinput').val('');
+            loadTweets();
+            $('.counter').text('140');
+      });
+  });
 
 });
-
-
-
-
 
 
 
