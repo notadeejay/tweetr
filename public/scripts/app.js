7145c9ccae625569d3bfefc1770f22916d2ab36c
Amy <amy@lighthouselabs.ca>
2017-08-08 16:45:35 +0000

    Added function to render new tweets on page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,101 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+var data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
+
+
+
+const generateHTML = (obj) => {
+  const html = `
+  <article>
+      <header>
+        <img class="usericon" src="${obj.user.avatars.small}">
+        <span class="username">${obj.user.name}</span>
+        <span class="user-handle">${obj.user.handle}</span>
+      </header>
+
+      <div class="tweet-content">
+        <span>${obj.content.text}</span>
+      </div>
+
+      <footer>
+        <span class="date">${convertDate(obj).created_at}<span>
+        <span class="edit-on-hover" role="img-icon">
+          <a href="#"><img class="hover-icon" src ="./images/flag.png"></a>
+          <a href="#"><img class="hover-icon" src ="./images/retweet.png"></a>
+          <a href="#"><img class="hover-icon" src ="./images/like.png"></a>
+        </span>
+      </footer>
+    </article>
+    `
+  return html
+}
+
+//DOM READY
+$(function() {
+
+
+const renderTweet= (data) => {
+  var articles = data.map(generateHTML)
+  var html = articles.join('')
+  $('#tweets-container').html(html)
+ }
+
+
+ renderTweet(data)
+
+});
+
+
+
+
+
+
+
+
+
+
+
 
---
+++ public/scripts/getDate.js
@@ -0,0 +1,37 @@
+function convertDate(obj) {
+  var timeSince = "";
+  var seconds = Math.floor((new Date() - obj.created_at) / 1000);
+  //CONVERT TO YEARS
+  var year = Math.floor(seconds / 31536000);
+  var months = Math.floor(seconds / 2592000);
+  var days = Math.floor(seconds / 86400);
+  var hours = Math.floor(seconds / 3600);
+  var minutes = Math.floor(seconds / 60);
+
+
+  if (year > 1) {
+  timeSince = year + 'years ago'
+  } else if (months > 1) {
+    timeSince = months + ' months ago';
+  } else if (days > 1) {
+    timeSince =  days + ' days ago';
+  } else if (hours > 1) {
+    timeSince =  hours + ' hours ago';
+  } else if (minutes > 1) {
+    timeSince =  minutes + ' minutes ago';
+  } else if (seconds < 5) {
+    timeSince = 'Just now';
+  } else {
+   timeSince =  Math.floor(seconds) + ' seconds ago';
+  }
+
+  obj = {
+    user: obj.user.name,
+    avatars: obj.user.avatars,
+    handle: obj.user.handle,
+    content: obj.content,
+    created_at: timeSince
+  }
+
+  return obj
+}
